cbe87bddb3b29d256ed07b500f3ebc7a583c5e34
Keith Rincon <keithrt3008@gmail.com>
2024-11-11 17:12:24 -0800

    Committing changes before running the Sentry wizard

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -10,6 +10,7 @@ import { formSchema } from '@/lib/validation';
 import { z } from 'zod';
 import { useToast } from '@/hooks/use-toast';
 import { useRouter } from 'next/navigation';
+import { createPitch } from '@/lib/actions';
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -30,18 +31,16 @@ const StartupForm = () => {
 
       await formSchema.parseAsync(formValues);
 
-      console.log(formValues);
-      // const result = await createIdea(prevState, formData, pitch);
-      // console.log(result);
+      const result = await createPitch(prevState, formData, pitch);
 
-      // if (result.status === 'SUCCESS') {
-      //   toast({
-      //     title: 'Success',
-      //     description: 'Your startup pitch has been created successfully',
-      //   });
-      //   router.push(`/startup/${result.id}`);
-      // }
-      // return result;
+      if (result.status == 'SUCCESS') {
+        toast({
+          title: 'Success',
+          description: 'Your startup pitch has been created successfully',
+        });
+        router.push(`/startup/${result._id}`);
+      }
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors;
@@ -73,7 +72,7 @@ const StartupForm = () => {
 
   const [state, formAction, isPending] = useActionState(handleFormSubmit, {
     error: '',
-    status: 'INITAL',
+    status: 'INITIAL',
   });
 
   return (
