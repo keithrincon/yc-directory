051b203cf6bbe3250230761fee97bf34f63c55e3
Keith Rincon <keithrt3008@gmail.com>
2024-11-12 14:03:18 -0800

    App Completed

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,13 +1,17 @@
-import { formatDate } from '@/lib/utils';
-import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
-import Image from 'next/image';
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
 import React, { Suspense } from 'react';
+import { client } from '@/sanity/lib/client';
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
+import { formatDate } from '@/lib/utils';
+import Link from 'next/link';
+import Image from 'next/image';
 import markdownit from 'markdown-it';
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 
 const md = markdownit();
 
@@ -16,9 +20,12 @@ export const experimental_ppr = true;
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  console.log({ id });
-
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: 'editor-picks-new',
+    }),
+  ]);
 
   if (!post) return notFound();
 
@@ -28,6 +35,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
     <>
       <section className='pink_container !min-h-[230px]'>
         <p className='tag'>{formatDate(post?._createdAt)}</p>
+
         <h1 className='heading'>This is a startup number: {post.title}</h1>
         <p className='sub-heading !max-w-5xl'>{post.description}</p>
       </section>
@@ -71,7 +79,19 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
           )}
         </div>
         <hr className='divider' />
-        {/*TODO: EDITOR SELECTED STARTUP */}
+
+        {editorPosts?.length > 0 && (
+          <div className='max-w-4xl mx-auto'>
+            <p className='text-30-semibold'>Editor Picks</p>
+
+            <ul className='mt-7 card_grid-sm'>
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
         <Suspense fallback={<Skeleton className='view_skeleton' />}>
           <View id={id} />
         </Suspense>
